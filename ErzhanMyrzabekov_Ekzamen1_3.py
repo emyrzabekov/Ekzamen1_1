46108ae02b9db5f3770082024779c1ddd1379638
Your Name <you@example.com>
2021-04-13 21:24:18 +0600

    initial commit

---
+++ ErzhanMyrzabekov_Ekzamen1_3.py
@@ -0,0 +1,29 @@
+# Задание 3:
+
+# def func_list(*args):
+#     return ['Element', 'start', *args, 'finish']
+# print(func_list(*['Hello', 5, 'go']))
+
+
+
+# def func_dict(*args):
+#     new_dict = []
+#     new_dict = dict(args)
+#     return args
+# print(func_dict('x', 5, 'John'))
+
+
+
+# def func_tuple(*args):
+#     odd_num = []
+#     vkvadrate = []
+#     i = 0
+#     for i in args:
+#         vkvadrat = i*i
+#         vkvadrate.append(vkvadrat)
+#         if i % 2 == 0:
+#             odd_num.append(i)
+#         i += 1
+#     print(odd_num)
+#     print(vkvadrate)
+# func_tuple(12,5,45,4,2,8,3)
